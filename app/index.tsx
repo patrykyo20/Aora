4dfb57da601d6a2e14670815f03ee304237aab8d
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-05-14 19:41:55 +0200

    change font in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function RootLayout() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-3xl'>Hello, moge kielona?</Text>
+      <Text className='text-3xl font-pblack'>Hello, moge kielona?</Text>
       <Link href={'profile'} style={{ color: 'blue' }}>
         Go to Profile
       </Link>
