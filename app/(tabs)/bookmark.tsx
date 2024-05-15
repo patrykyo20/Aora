6bb4ab36962420e26ae72a8c79498f5730144df2
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-05-15 15:50:47 +0200

    create  bookmark template

---
+++ app/(tabs)/bookmark.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Bookmark = () => {
+  return (
+    <View>
+      <Text className="text-3xl" >Bookmark</Text>
+    </View>
+  )
+}
+
+export default Bookmark
+
+const styles = StyleSheet.create({})
