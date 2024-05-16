4733fa6a168c0be8706e70fe4fc45d3c2f9c1708
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-05-16 08:40:39 +0200

    Create FormField component

---
+++ components/FormField.tsx
@@ -0,0 +1,63 @@
+import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native'
+import { FC, useState } from 'react'
+import { icons } from '@/constants';
+
+interface FormFieldProps {
+  title: string;
+  value: string;
+  placeholder: string;
+  handleChangeText: (text: any) => void;
+  otherStyles: string;
+  props: any;
+}
+
+const FormField: FC<FormFieldProps> = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className={`space-y-2 ${otherStyles}`}> 
+      <Text
+        className='text-base text-gray-100 font-pmedium'
+      >
+        {title}
+      </Text>
+
+      <View
+        className='border-2 border-black-200 w-full h-16
+          px-4 bg-black-100 rounded-xl flex-row
+          focus:border-secondary-100 items-center'
+      >
+        <TextInput
+          className='flex-1 text-white font-psemibold
+            text-base'
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor='#7b7b8b'
+          onChange={handleChangeText}
+          secureTextEntry={title === 'Password' && !showPassword}
+        />
+
+        {title === 'Password' && (
+          <TouchableOpacity onPress={() => {
+            setShowPassword(!showPassword);
+          }}>
+            <Image 
+              source={!showPassword ? icons.eye : icons.eyeHide }
+              className='w-6 h-6'
+              resizeMode='contain'
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  )
+}
+
+export default FormField;
