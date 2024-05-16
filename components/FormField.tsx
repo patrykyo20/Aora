1d681172e1becef3164e9c8796f2f77915840a14
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-05-16 13:35:01 +0200

    change TextInput in FormField component

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -24,42 +24,32 @@ const FormField: FC<FormFieldProps> = ({
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <View className={`space-y-2 ${otherStyles}`}> 
-      <Text
-        className='text-base text-gray-100 font-pmedium'
-      >
-        {title}
-      </Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
 
-      <View
-        className='border-2 border-black-200 w-full h-16
-          px-4 bg-black-100 rounded-xl flex-row
-          focus:border-secondary-100 items-center'
-      >
+      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
         <TextInput
-          className='flex-1 text-white font-psemibold
-            text-base'
+          className="flex-1 text-white font-psemibold text-base"
           value={value}
           placeholder={placeholder}
-          placeholderTextColor='#7b7b8b'
-          onChange={handleChangeText}
-          secureTextEntry={title === 'Password' && !showPassword}
+          placeholderTextColor="#7B7B8B"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+          {...props}
         />
 
-        {title === 'Password' && (
-          <TouchableOpacity onPress={() => {
-            setShowPassword(!showPassword);
-          }}>
-            <Image 
-              source={!showPassword ? icons.eye : icons.eyeHide }
-              className='w-6 h-6'
-              resizeMode='contain'
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
             />
           </TouchableOpacity>
         )}
       </View>
     </View>
-  )
+  );
 }
 
 export default FormField;
