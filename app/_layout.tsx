098f9f8291e976a172bf0681c17b12d1f0ed62ac
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-05-16 15:04:37 +0200

    Change headerShown to false in Stack.Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,7 +32,12 @@ const RootLayout = () => {
         <Stack.Screen name='index' options={{
           headerShown: false,
         }}/>
+
         <Stack.Screen name='(auth)' options={{
+          headerShown: false,
+        }}/>
+
+        <Stack.Screen name='(tabs)' options={{
           headerShown: false,
         }}/>
         
